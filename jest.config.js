1505890ebc1fcb0a2e4a83639d86162d3042a8a9
Anthony Cook <ddragonmaster49@yahoo.com>
2021-11-04 11:11:51 -0400

    Update babel and jest configs

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,9 @@ const config = {
       "identity-obj-proxy",
   },
   setupFilesAfterEnv: ["<rootDir>/jest-setup.js"],
+  transform: {
+    "^.+\\.[t|j]sx?$": "babel-jest",
+  },
 };
 
-module.exports = config;
+export default config;
